207bdcb1b720aaca46f9a595838df45dafd516f2
chris torok <torok.chris@gmail.com>
2020-05-26 19:18:51 -0400

    first commit

---
+++ js_sandbox/app.js
@@ -0,0 +1,18 @@
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// //Init
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// LET
+// let name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+const 
