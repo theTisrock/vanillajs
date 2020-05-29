44f15a86a9be84c757f2c0c2465beb18dc275315
chris torok <torok.chris@gmail.com>
2020-05-28 20:37:38 -0400

    math

---
+++ js_sandbox/data_types/math.js
@@ -0,0 +1,26 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+// Simple math with numbers
+
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);  // automatic rounding
+val = Math.ceil(2.01);  // rounds to next whole int
+val = Math.floor(2.9);  // rounds to previous whole int
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(2, 3);
+val = Math.min(1, 2, 3, 4, 5, 6, 7);
+val = Math.max(1, 2, 3, 4, 5, 6, 7);
+val = Math.random();
+
+console.log(val);
