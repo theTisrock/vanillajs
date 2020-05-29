1c479e8b8cc8cdb92e57e74a3385b14c842293b1
chris torok <torok.chris@gmail.com>
2020-05-28 20:35:53 -0400

    data types

---
+++ js_sandbox/data_types/dataTypes.js
@@ -0,0 +1,42 @@
+/* PRIMITIVE */
+
+// String 
+const name = 'John Doe';
+console.log(typeof name);
+
+// number
+const age = 5;
+console.log(typeof age);
+
+// Boolean
+const hasKids = true;
+console.log(typeof hasKids);
+
+// null
+const car = null;
+console.log(typeof car); /* returns type 'object'; this is a bug in Javascript */
+
+// undefined
+let test;
+console.log(typeof test);
+
+// symbol
+const sym = Symbol();
+console.log(typeof sym);
+
+
+/* REFERENCE - all objects */
+
+// Array
+const hobbies = ['movies', 'music'];
+console.log(typeof hobbies);
+
+// Object literal
+const address = {
+    city: 'Boston',
+    state: 'MA'
+}
+console.log(typeof address);
+
+// Date
+const today = new Date();
