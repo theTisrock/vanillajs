217966e5863ed60536676196449eb5e8e59484a4
chris torok <torok.chris@gmail.com>
2021-01-28 18:00:44 -0500

    showing the basic approach for creating the app: registering events, defining components, creating handlers, creating single purpose functions, and reusing those declaratively

--- TaskList/app.js
+++ TaskList/app.js
@@ -1,4 +1,9 @@
-
+/* CHALLENGE: try to make these components as decoupled as possible
+    
+    considerations for functionality:
+    
+    - need to be able to add <li> elements to the list
+*/
 
 // Define UI vars (components)
 const form = document.querySelector('#task-form');
@@ -7,11 +12,56 @@ const clearBtn = document.querySelector('a.clear-tasks');
 const taskFilter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
+// register events
+function loadEventListeners() {
+    // component.addEventListener('event type', handler);
+}
 
+
+function createListItem(contentText, href="#", classes = "", id = "") {
+    
+    /* create a list item */
+    let li = document.createElement('li');
+    li.textContent = contentText;
+
+    // add options
+    li.setAttribute('href', href);
+
+    if (classes) {  // empty strings are falsey
+        let classList = classes.split(" ");
+        console.log(classList);
+        classList.forEach(function(element) {
+            li.classList.add(`${element}`)
+        })
+    };
+
+    if (id) { li.setAttribute('id', id)};
+    
+    // deliver result
+    return li;
+}
+function addTask(event, taskName) {
+    /* add a task to the list in the way that we need things to be done */
+    createListItem(taskName)
+}
+
+
+
+/* TEST SUITE */
+console.log("TEST SUITE");
 // validate components
 console.log(taskInput);
 console.log(taskFilter);
 console.log(clearBtn);
 console.log(tasksList);
 console.log(form);
+
+// validate functions
+console.log(createListItem("HELLO THERE!"));
+console.log(createListItem("Custom href", 'not #'));
+console.log(createListItem("custom class list", "#", "class1 class2", "customid"));
+
+console.log("END TESTS");
+
+
 
--- TaskList/todos.md
+++ TaskList/todos.md
@@ -6,6 +6,9 @@ Boilerplate:
     2) cdn materialize js -> cdn jquery Javascript lib
     2) font awesome
 
+
+Build the App:
 * app.js file (our main js file)
     1) define variables/components that you know you'll need to work with in the DOM.
-    
+    2) decide on functions and list them out
+    3) centralize eventListener registration
