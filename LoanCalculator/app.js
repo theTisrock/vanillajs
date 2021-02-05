dfd5c844d76090c9b0ac35d3601c350bd56245b2
chris torok <torok.chris@gmail.com>
2021-02-05 13:27:09 -0500

    finished calculation section; separated the high-level event listener code from the loan calculation. Brad has everything under the event listener, which I consider to be too tightly coupled.

--- LoanCalculator/app.js
+++ LoanCalculator/app.js
@@ -1,54 +1,60 @@
 const loanForm = document.getElementById("loan-form");
-console.log(loanForm);
 
 
 function getLoanRepaymentDetails(loanAmount, interestRate, loanLengthInYears) {
+
+    let principle = loanAmount;
+    let calculatedInterest = interestRate / 100 / 12;
+    let calculatedPayments = loanLengthInYears * 12;
+    let x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    let monthly = (principle * x * calculatedInterest) / (x-1);
+    let total = monthly * calculatedPayments;
+    let totalInt = (monthly * calculatedPayments) - principle;
+
     const results = {
-        monthlyPayment: "",
-        totalPayment: "",
-        totalInterest: "",
+        monthlyPayment: monthly,
+        totalPayment: total,
+        totalInterest: totalInt,
     };
 
-    // main driver for calculation event here will use helper functions
-
-    return results;
+    return results;  // feed this into UI
 }
+
 
 /* Events */
 function calculateEvent(event) {
     event.preventDefault();
-    // console.log('Calculated!');
 
     let loanAmount = document.getElementById("amount").value;  // obtain values
     let interestRate = document.getElementById("interest").value;
     let lifetimeYears = document.getElementById("years").value;
 
     const hasRequiredParameters = loanAmount != "" && interestRate != "" && lifetimeYears != "";  // check all values are supplied
-    if (!hasRequiredParameters)
+    if (!hasRequiredParameters) {
         alert("Not all loan parameters have been supplied!");
+        return;
+    }
 
-    loanAmount = Number(loanAmount);  // prepare data for calculation
-    interestRate = Number(interestRate);
-    lifetimeYears = Number(lifetimeYears);
+    loanAmount = parseFloat(loanAmount);  // prepare data
+    interestRate = parseFloat(interestRate);
+    lifetimeYears = parseFloat(lifetimeYears);
 
-    const results = calculateLoanValues(loanAmount, interestRate, lifetimeYears);  // calculate
+    const results = getLoanRepaymentDetails(loanAmount, interestRate, lifetimeYears);  // calculate
 
     let monthlyPayment = document.getElementById("monthly-payment");
     let totalPayment = document.getElementById("total-payment");
     let totalInterest = document.getElementById("total-interest");
 
-    // console.log(monthlyPayment, totalPayment, totalInterest);  // verify
-
-    monthlyPayment.setAttribute('value', results.monthlyPayment);  // output
-    totalPayment.setAttribute('value', results.totalPayment);
-    totalInterest.setAttribute('value', results.totalInterest);
+    monthlyPayment.setAttribute('value', results.monthlyPayment.toFixed(2));  // output
+    totalPayment.setAttribute('value', results.totalPayment.toFixed(2));
+    totalInterest.setAttribute('value', results.totalInterest.toFixed(2));
 }
 
 
-/* Event Listeners */
 function loadEventListeners() {
     loanForm.addEventListener('submit', calculateEvent);
 }
+
 
 // init
 loadEventListeners();
