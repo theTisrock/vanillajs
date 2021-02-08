0ea2a8b30bb67b8604ded4741ff9ef7720a98c80
chris torok <torok.chris@gmail.com>
2021-02-08 12:48:28 -0500

    Temporarily removed random number generation to aid in debugging; implemented a showGameOver logic

--- NumberGuesser/app.js
+++ NumberGuesser/app.js
@@ -38,6 +38,12 @@
                             parseInt("100") -> 100 
                             parseInt("100x") -> 100
                             parseInt("10", 16) -> 16
+
+        - I need to focus on the workflow of the app first instead of optimizing first,
+            for example, don't worry about generating a random number, but use a placeholder winning number instead
+            Use dummy data to insert in the application work flow. Then mark what needs to be optimized.
+        
+        - settings should be decoupled from the app and not stored inside it. The app is the slave to the settings. The settings should not be slave to the app.
 */
 
 const settings = {  // stuff that should not change when the app is running
@@ -49,15 +55,19 @@ const settings = {  // stuff that should not change when the app is running
 const app = {
     data: {  // the variables to use for internal app logic
         guessCount: 0,
-        winningNumber: Math.floor(Math.random() * settings.upperBound),
+        winningNumber: 2, // Math.floor(Math.random() * settings.upperBound),
+        gameOver: false,
     },
     // methods - functions that modify that app data
     methods: {
         incrementGuessCount: function() {
             app.data.guessCount += 1;
         },
-        setSecretNumber: function() {
-            app.data.winningNumber = Math.floor(Math.random() * settings.upperBound);
+        setWinningNumber: function() {
+            app.data.winningNumber = 2; // Math.floor(Math.random() * settings.upperBound);
+        },
+        guessesLeft: function() {
+            return settings.maxGuessCount - app.data.guessCount;
         },
     }, 
     func: {
@@ -72,7 +82,7 @@ const app = {
         },
     },
     init: function() {
-        app.methods.setSecretNumber();
+        app.methods.setWinningNumber();
     },
 };
 
@@ -83,6 +93,29 @@ userInstructions.querySelector('.min-num').textContent = settings.lowerBound;
 userInstructions.querySelector('.max-num').textContent = settings.upperBound;
 
 
+function showGameOver(won) {
+    let color = (won === true) ? "green" : "red";
+    let msg = (won === true) ? 
+    "You won!" : `Woops! The correct answer is ${app.data.winningNumber}`;
+    const userInputArea = document.querySelector('#guess-input');
+    const message = document.querySelector('.message');
+    // lock the input box
+    userInputArea.disabled = true;
+    // display the message
+    message.textContent = msg;
+    // apply feedback color
+    userInputArea.style.border = color;
+    message.style.color = color;
+}
+
+
+function showCountDown() {
+    const guessesLeft = app.methods.guessesLeft();
+    const message = document.querySelector('.message');
+    message.textContent = `You have ${guessesLeft} guesses left.`
+}
+
+
 function playGameEvent(event) {  // main driver
 
     if (event.target.id === "guess-submit") {
@@ -90,20 +123,27 @@ function playGameEvent(event) {  // main driver
         const guessesLeft = settings.maxGuessCount - app.data.guessCount;
         const guessInput = game.querySelector('#guess-input');
         const guess = parseInt(guessInput.value);
+        let outcome = false;
 
         const validInput = !isNaN(guess) && guess >= settings.lowerBound && guess <= settings.upperBound;
         if (!validInput) {
             alert(`Only enter numbers between ${settings.lowerBound} and ${settings.upperBound}`);
         }
         else if (app.data.guessCount < settings.maxGuessCount) {
-                // const guess = Number(guessInput.value);
-                const outcome = app.func.guessAttempt(guess, settings.lowerBound, settings.upperBound);
-                message.textContent = (outcome) ? "win" : "lose";
-        } else {
+                // const guess = Number(guessInput.value); changed to parseInt above
+                outcome = app.func.guessAttempt(guess, settings.lowerBound, settings.upperBound);
+        } 
+        else {
             message.textContent = `${guessesLeft} guesses left.`;
         }
 
         app.methods.incrementGuessCount(1);
+
+        let gameOver = (outcome === true || app.data.guessCount >= settings.maxGuessCount);
+        if (gameOver === true)
+            showGameOver(outcome);
+        else
+            showCountDown();
     }
 }
 
