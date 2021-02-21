1edaf9e6df369e85696ff0a15f6d1a7b9cb5f5c8
chris torok <torok.chris@gmail.com>
2021-02-21 17:26:06 -0500

    create Book class (ES5), created app data model for storing a books list, mapped app data book list to table in DOM, enabled adding books to app via app.method & book form submit event

--- BookList/app.js
+++ BookList/app.js
@@ -1,84 +1,109 @@
-/* ES 5 book application */
 
-/* we need:
-
- 1. book app with a data model
- 2. book objects
- 2. communication layer between DOM and app and app to DOM
-
- */
-
+// Book class (es5 style)
 function Book(title, author, isbn) {
     this.title = title;
     this.author = author;
     this.isbn = isbn;
 }
 
-const mockingbird = new Book("To Kill A Mockingbird", "Harper Lee", 12345);
-const mountain = new Book("My Side of the Mountain", "Jean Craighead George", 54321);
-const orwell = new Book("1984", "George Orwell", 56789);
+// test
+// const book = new Book("To Kill A Mockingbird", "Harper Lee", 999);
+// console.log(book);
+
+// mock data
+const mockingbird = new Book("To Kill A Mockingbird", "Harper Lee", 999);
+const moutain = new Book("My Side of the Mountain", "Jean Craighead George", 888);
+const drseuss = new Book("Green Eggs & Ham", "Dr. Suess", 777);
+
+// ui
+const bookForm = document.querySelector('#book-form');
 
 
 const app = {
     data: {
-        books: [mockingbird, mountain, orwell],
+        books: [mockingbird, moutain, drseuss],
     },
     methods: {
-        createBook: function(title, author, isbn) {
-            return new Book(title, author, isbn);
+        clearAllBooks: function() {
+            app.data.books = [];
         },
         addBook: function(newBook) {
-            app.data.books.append(newBook);
-        },
-        deleteBook: function() {
-            return null;
-        },
-        clearAllBooks: function() {
-            return null;
+            app.data.books.push(newBook);
+            refreshBookTable();
         },
     },
 }
 
-// App to DOM
-function clearBooksTable() {
-    const tableBody = document.querySelector('#books-collection');
-    tableBody.querySelectorAll('tr').forEach(function(row) {
-        row.remove();
-    })
-}
 
-function renderBooksTable() {
-
-    // clear the UI
-    clearBooksTable();
-
-    const books = app.data.books;
-    const tableBody = document.querySelector('#books-collection');
-
-    // apply current app data state to UI
-    books.forEach(function(bookObject) {
-        const newTableRow = document.createElement('tr');
-
-        const deleteBtn = document.createElement('button');
-        deleteBtn.setAttribute('type', 'submit');
-
-        const title = document.createElement('td');
-        const author = document.createElement('td');
-        const isbn = document.createElement('td');
-        const del = document.createElement('td');
-
-        title.textContent = bookObject.title;
-        author.textContent = bookObject.author;
-        isbn.textContent = bookObject.isbn;
-        del.innerHTML = `<button>delete</button>`;
-
-        newTableRow.appendChild(title);
-        newTableRow.appendChild(author);
-        newTableRow.appendChild(isbn);
-        newTableRow.appendChild(del);
-
-        console.log(newTableRow);
-
-        tableBody.appendChild(newTableRow);
+function clearAllBooksFromTable() {
+    const tbody = document.querySelector('#books-collection');
+    tbody.querySelectorAll('td').forEach(function(tableDataEntry) {
+        tableDataEntry.remove();
     });
 }
+
+
+function refreshBookTable() {
+    clearAllBooksFromTable();
+
+    const books = app.data.books;  // app data
+    console.log(books);
+    const tbody = document.querySelector('#books-collection');  // acquire DOM section
+
+    // for each entry in books list
+    books.forEach(function(bookObject) {
+        const row = document.createElement('tr');
+
+        const title = document.createElement('td');
+        title.textContent = bookObject.title;
+        const author = document.createElement('td');
+        author.textContent = bookObject.author;
+        const isbn = document.createElement('td');
+        isbn.textContent = bookObject.isbn;
+        const delBtn = document.createElement('td');
+        delBtn.innerHTML = `<button type="submit">delete</>`;
+
+        row.appendChild(title);
+        row.appendChild(author);
+        row.appendChild(isbn);
+        row.appendChild(delBtn);
+
+        tbody.appendChild(row);
+    });
+}
+
+
+/* events & listeners */
+function onLoadEvent(event) {
+    refreshBookTable();
+}
+
+function addBookEvent(event) {
+    event.preventDefault();
+    // extract data
+    const title = bookForm.querySelector('#title').value;
+    const author = bookForm.querySelector('#author').value;
+    const isbn = bookForm.querySelector('#isbn').value;
+
+    // console.log(title, author, isbn);  // test
+
+    // validate not null and not undefined
+    const noNulls = (title != null && author != null && isbn != null);
+    const allDefined = (typeof title != "undefined" && typeof author != "undefined" && typeof isbn != "undefined");
+    const validInputs = noNulls && allDefined;
+
+    if (validInputs) {
+        // instantiate a new book
+        const newBook = new Book(title, author, isbn);
+        // add book to app data book list
+        app.methods.addBook(newBook);
+    } else {
+        alert("Bad inputs in book form.")
+    }
+}
+
+
+(function loadEventListeners() {
+    document.addEventListener('DOMContentLoaded', onLoadEvent);
+    bookForm.addEventListener('submit', addBookEvent);
+})();
---
+++ BookList/todo.md
@@ -0,0 +1,10 @@
+TODOS:
+
+*Construct a class to encapsulate and instantiate Book instances: x
+*Store book instances inside an app data model that houses a list of books: x
+*Map the app's book data to the DOM in a table: x
+*Enable adding books to the list by accepting via the Add Book form and sending validated data back to the app data books list: x
+    - create app method addBook(newBook)
+    - user addBook(newBook) to accept form data via click event
+*Enable the deletion of all books
+*Enable the deletion of a target book
