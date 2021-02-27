74f7dff85c66ac51e5f684ed6b7d11d244f7a3e2
chris torok <torok.chris@gmail.com>
2021-02-27 14:07:33 -0500

    using xhr objects with callback functions for ajax requests

--- asyncAndFetch/ajaxAndJSON.js
+++ asyncAndFetch/ajaxAndJSON.js
@@ -1,1 +1,74 @@
-console.log("hello");
+
+// ui
+const getCustomerBtn = document.getElementById('button1');
+const customerOutput = document.querySelector('#customer');
+const getCustomersBtn = document.getElementById('button2');
+const customersOutput = document.querySelector('#customers');
+
+
+// app behavior
+function displayCustomer(customerData) {
+    const structuredOutput = document.createElement('p');
+    structuredOutput.textContent = `Profile: ${customerData.id}, ${customerData.name}, ${customerData.phone}, ${customerData.phone}`;
+    customerOutput.appendChild(structuredOutput);
+}
+
+function displayCustomers(customersData) {
+    const structuredOutput = document.createElement('ol');
+
+    customersData.forEach(function(customer, index) {
+        let li = document.createElement('li');
+        li.textContent = `id: ${customer.id} name: ${customer.name} company: ${customer.company} phone: ${customer.phone}`;
+        structuredOutput.appendChild(li);
+    });
+
+    customersOutput.appendChild(structuredOutput);
+}
+
+function getCustomer() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'customer.json', true);
+    
+    xhr.onload = function() {
+        const responseOk = this.status >= 200 && this.status <= 299;
+        if (responseOk) {
+            const customerData = JSON.parse(this.responseText);
+            displayCustomer(customerData);
+        }
+    }
+
+    xhr.send();
+}
+
+function getCustomers() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'customers.json', true);
+
+    xhr.onload = function() {
+        const responseOk = this.status >= 200 && this.status <= 299;
+        if (responseOk) {
+            const customersData = JSON.parse(this.responseText);
+            displayCustomers(customersData);
+        }
+    }
+
+    xhr.send();
+}
+
+
+// Events
+function getCustomerEvent(event) {
+    getCustomer();
+}
+
+function getCustomersEvent(event) {
+    getCustomers();
+}
+
+
+function loadEventListeners() {
+    getCustomerBtn.addEventListener('click', getCustomerEvent);
+    getCustomersBtn.addEventListener('click', getCustomersEvent);
+}
+
+loadEventListeners();
