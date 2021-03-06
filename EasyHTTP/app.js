86039056ad4e56cef6772c7d564358e6bf0cf8b7
chris torok <torok.chris@gmail.com>
2021-03-06 16:48:48 -0500

    finished get, put, post, delete methods and response handler; moved internal response parameter feeder to it's own function

--- EasyHTTP/app.js
+++ EasyHTTP/app.js
@@ -5,9 +5,35 @@ const http = new easyHTTP;
 // console.log(posts);  // returns undefined, the async response has not been handled
 
 
-function responseHandler(error, response, status) {
+function responseHandler(error, response, status, method) {
     const responseOk = status >= 200 && status < 299;
+    console.log(`${method}, ${status}`);
     responseOk ? console.log(response) : console.log(error);
 }
 
+
+
+// READ
+// get resources
 http.get('http://jsonplaceholder.typicode.com/posts', responseHandler);
+http.get('http://jsonplaceholder.typicode.com/posts/1', responseHandler)
+
+
+// WRITE
+const myData = {
+    title: "my title",
+    body: "my body",
+    userId: 1,
+}
+
+// create a resource
+http.post('http://jsonplaceholder.typicode.com/posts', myData, responseHandler);
+
+
+// replace a resource
+http.put('http://jsonplaceholder.typicode.com/posts/1', myData, responseHandler);
+
+
+// DELETE
+// remove a resource
+http.delete('http://jsonplaceholder.typicode.com/posts/1', responseHandler);
--- EasyHTTP/easyhttp.js
+++ EasyHTTP/easyhttp.js
@@ -1,5 +1,13 @@
 
 
+function _handleResponse(self, method, responseHandlerCallback) {
+    const STATUS = self.http.status;
+    const response = self.http.responseText;
+    const error = `Error: ${STATUS}`;
+
+    responseHandlerCallback(error, response, STATUS, method);
+}
+
 
 // constructor wrapper for XHR object
 function easyHTTP() {
@@ -9,30 +17,63 @@ function easyHTTP() {
 
 // Make an HTTP GET Request
 easyHTTP.prototype.get = function(url, responseHandlerCallback) {
-    this.http.open('GET', url, true);  // connection context, asynchronous
+    const method = 'GET'
+    this.http.open(method, url, true);  // connection context, asynchronous
+    
     let self = this;  // lexical scope passed into the function below.
-
-    this.http.onload = function() {  // the plan when the response is returned
-        const STATUS = self.http.status;
-        const response = self.http.responseText;
-        const error = `Error: ${STATUS}`;
-
-        responseHandlerCallback(error, response, STATUS);
-    }
+    this.http.onload = function() {
+        _handleResponse(self, method, responseHandlerCallback);
+    };
 
     this.http.send();
 }
 
 // Make an HTTP POST Request
-easyHTTP.prototype.post = function(url) {  // create a resource
+easyHTTP.prototype.post = function(url, dataObject, responseHandlerCallback) {  // create a resource
+    // prepare request
+    const method = 'POST';
+    const data = JSON.stringify(dataObject);
+    this.http.open(method, url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+    
+    let self = this;
+    // SEE IF YOU CAN MAKE BELOW MORE DRY
+    this.http.onload = function() {
+        _handleResponse(self, method, responseHandlerCallback);
+    };
+
+    this.http.send(data);
 }
 
 // Make an HTTP PUT Request
-easyHTTP.prototype.put = function(url) {  // replace a resource
+easyHTTP.prototype.put = function(url, dataObject, responseHandlerCallback) {  // replace a resource
+    // prepare request
+    const method = 'PUT';
+    const data = JSON.stringify(dataObject);
+    this.http.open(method, url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
     
+    let self = this;
+    // SEE IF YOU CAN MAKE BELOW MORE DRY
+    this.http.onload = function() {
+        _handleResponse(self, method, responseHandlerCallback);
+    };
+
+    this.http.send(data);
 }
 
 // Make an HTTP DELETE Request
-easyHTTP.prototype.delete = function(url) {
-    
+easyHTTP.prototype.delete = function(url, responseHandlerCallback) {
+    // prepare the request
+    const method = 'DELETE';
+    this.http.open('DELETE', url, true);
+
+    // prepare response handling
+    let self = this;
+    this.http.onload = function() {
+        _handleResponse(self, method, responseHandlerCallback);
+    };
+
+    this.http.send();
 }
+ 
