50ad14d8f1314001e5262c171135477b84a3692a
Chris Torok <torok.chris@gmail.com>
2021-04-15 20:32:59 -0400

    Trying to drive the app development from the driver module; also Listener setup

--- GithubFinder/app.js
+++ GithubFinder/app.js
@@ -1,1 +1,25 @@
 // Main driver for web application
+
+
+// external resources
+const githubClient = new Github;
+const ui = new UI;
+
+
+// events & listeners ONLY - 
+// do not manipulate the DOM directly here; only use this area for events
+// app data & UI code should respond to profile search events (keyup)
+const searchUserInput = document.querySelector('#searchUser');
+
+
+function searchProfiles(event) {
+    console.log("searchProfiles was triggered");
+}
+
+function loadEventListeners() {
+    searchUserInput.addEventListener('keyup', searchProfiles);
+}
+
+
+// initialize
+loadEventListeners();
