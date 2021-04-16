1028eb9d394a47b9f562ef4f80c4770add525018
Chris Torok <torok.chris@gmail.com>
2021-04-15 20:33:38 -0400

    created skeleton worker classes

--- GithubFinder/github.js
+++ GithubFinder/github.js
@@ -1,1 +1,15 @@
 // github http client for interfacing with github web api
+
+// TODO: Set up this application with Github API key
+
+class Github {
+
+    constructor() {
+
+    }
+
+    async getProfile() {
+
+    }
+
+}
--- GithubFinder/ui.js
+++ GithubFinder/ui.js
@@ -1,1 +1,17 @@
 // worker for DOM manipulation, slave to app.js
+
+class UI {
+
+    constructor() {
+        // components to be manipulated
+    }
+
+    showProfile() {
+
+    }
+
+    showAlert() {
+        
+    }
+
+}
