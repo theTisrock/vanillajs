3990d6c7e6276622daba298057077f83c7127e4c
Chris Torok <torok.chris@gmail.com>
2021-04-15 20:52:09 -0400

    Added driver pseudocode for marrying data and ui together; attempting to drive development from the app entry point

--- GithubFinder/app.js
+++ GithubFinder/app.js
@@ -9,11 +9,26 @@ const ui = new UI;
 // events & listeners ONLY - 
 // do not manipulate the DOM directly here; only use this area for events
 // app data & UI code should respond to profile search events (keyup)
+
+// only use DOM components here for the purpose of registering events
+// no DOM manipulation should be done in this module.
 const searchUserInput = document.querySelector('#searchUser');
 
 
 function searchProfiles(event) {
     console.log("searchProfiles was triggered");
+
+    /* PSEUDO CODE BLOCK
+    const response = githubClient.getProfile();
+
+    if (!response.ok) {
+        ui.showAlert("Profile does not exist.");
+        return;
+    }
+
+    const data = response.data();
+    ui.showProfile(data);
+    */
 }
 
 function loadEventListeners() {
