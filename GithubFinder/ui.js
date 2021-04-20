252ebec2b59c076c1736517a24c1072709989f3b
Chris Torok <torok.chris@gmail.com>
2021-04-20 13:56:38 -0400

    meaningful variable name

--- GithubFinder/ui.js
+++ GithubFinder/ui.js
@@ -9,34 +9,34 @@ class UI {
     }
 
 
-    showProfile(responseData) {
+    showProfile(user) {
 
         this.profile.innerHTML = '';
 
-        if (typeof responseData.login !== "undefined") {  // basing the decision on login could be fragile, but it works.
+        if (typeof user.login !== "undefined") {  // basing the decision on login could be fragile, but it works.
             this.profile.innerHTML = `
                 <div class="card card-body mb-3">
                     <div class="row">
                         <div class="col-md-3">
-                            <img class="img-fluid mb-2" src=${responseData.avatar_url} />
-                            <a href="${responseData.html_url}" target="_blank" class="btn btn-primary btn-block">
+                            <img class="img-fluid mb-2" src=${user.avatar_url} />
+                            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block">
                             View Profile
                             </a>
                         </div>
 
                         <div class="col-md-9">
-                            <span class="badge badge-primary">Public Repos: ${responseData.public_repos}</span>
-                            <span class="badge badge-secondary">Public Gists: ${responseData.public_gists}</span>
-                            <span class="badge badge-success">Followers: ${responseData.followers}</span>
-                            <span class="badge badge-info">Following: ${responseData.following}</span>
+                            <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
+                            <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
+                            <span class="badge badge-success">Followers: ${user.followers}</span>
+                            <span class="badge badge-info">Following: ${user.following}</span>
 
                             <br />
 
                             <ul class="list-group">
-                                <li class="list-group-item">Company: ${responseData.company}</li>
-                                <li class="list-group-item">Website/Blog: ${responseData.blog}</li>
-                                <li class="list-group-item">Location: ${responseData.location}</li>
-                                <li class="list-group-item">Member Since: ${responseData.created_at}</li>
+                                <li class="list-group-item">Company: ${user.company}</li>
+                                <li class="list-group-item">Website/Blog: ${user.blog}</li>
+                                <li class="list-group-item">Location: ${user.location}</li>
+                                <li class="list-group-item">Member Since: ${user.created_at}</li>
                             </ul>
                         </div>
                     </div>
