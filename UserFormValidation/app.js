c4c644d4d0db0c37b9fcba4de567078f76caebe9
Chris Torok <torok.chris@gmail.com>
2021-04-22 14:37:25 -0400

    register event handlers with their corresponding functions

--- UserFormValidation/app.js
+++ UserFormValidation/app.js
@@ -12,3 +12,33 @@ const userFormInputs = {  // inputs from the ui/DOM
 
 
 // event listeners
+/* 
+Design: 
+    I want to build type-along validation for each individual field 
+    in addition to validation upon submission
+
+    Each field will need an event listener
+*/
+
+
+// event handlers
+nameValidator = (event) => console.log("nameValidator()");
+zipValidator = (event) => console.log("zipValidator()");
+emailValidator = (event) => console.log("emailValidator()");
+phoneValidator = (event) => console.log("phoneValidator()");
+validateForm = (event) => console.log("validateForm()");
+
+
+// register events
+function loadEventListeners() {
+    userFormInputs.name.addEventListener('keyup', nameValidator);
+    userFormInputs.zip.addEventListener('keyup', zipValidator);
+    userFormInputs.email.addEventListener('keyup', emailValidator);
+    userFormInputs.phone.addEventListener('keyup', phoneValidator);
+
+    userForm.addEventListener('submit', validateForm);
+}
+
+
+// init
+loadEventListeners();
